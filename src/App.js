ff2a440f08146df8459888154d4e6773d751082a
anasam31 <navratil.ana@gmail.com>
2024-04-28 14:11:54 +0200

    added api

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
-        <footer>
-          <a
-            href="https://github.com/ana-atn/weather-react-2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-source code
-          </a>{" "}
-          by ana atanackovic
-        </footer>
       </div>
+      <footer>
+        <a
+          href="https://github.com/ana-atn/weather-react-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-source code
+        </a>{" "}
+        by ana atanackovic
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,94 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./Weather.css";
+import Search from "./images/search.svg";
+import SearchLocation from "./images/search-location.svg";
+import SunCloud from "./images/sun-cloud.png";
 
 export default function Weather() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState(null);
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  function showWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: Math.round(response.data.main.temp),
-      description: response.data.weather[0].description,
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
-      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      tempmax: response.data.main.temp_max,
+      tempmin: response.data.main.temp_min,
+      date: "Friday, Aug 4, 17:24",
     });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    let apiKey = "3f6be1c407b0d9d1933561808db358ba";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
-
-  if (loaded) {
+  if (weatherData.ready) {
     return (
-      <div>
-        {form}
-        <ul>
-          <li> Temperature: {weather.temperature}°C </li>
-          <li> Description: {weather.description} </li>
-          <li> Humidity: {weather.humidity}% </li>
-          <li> Wind: {weather.wind} m/s </li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
+      <div className="Weather">
+        <div className="row city">
+          <div className="col current-city">{weatherData.city}</div>
+          <form className="col searchbar">
+            <input
+              className="bar form-control"
+              type="text"
+              placeholder="Search city..."
+              size="20"
+              autoFocus="on"
+              autoComplete="off"
+            />
+            <input className="go-button" type="image" src={Search} />
+            <button className="my-location-button">
+              <img src={SearchLocation} />
+            </button>
+          </form>
+        </div>
+        <div className="current-date-time" i>
+          {weatherData.date}
+        </div>
+        <div className="current-data">
+          <div className="row">
+            <div className="col temp-current">
+              <img src={SunCloud} width="70px" alt="Weather Icon" />
+              <span>{Math.round(weatherData.temperature)}</span>
+              <span className="c-f">
+                <span>
+                  <a href="#" className="tempconversion-link">
+                    {" "}
+                    °C
+                  </a>
+                </span>
+                |
+                <span>
+                  <a href="#" className="tempconversion-link">
+                    {" "}
+                    °F{" "}
+                  </a>
+                </span>
+              </span>
+              <br />
+              <div className="temp-current-min-max">
+                <span className="temp-max">
+                  {Math.round(weatherData.tempmax)}
+                </span>
+                /<span>{Math.round(weatherData.tempmin)}</span>
+              </div>
+            </div>
+            <div className="col current-other-info">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     );
   } else {
-    return form;
+    const apiKey = "73a00877081bd43422bdee0f3022beb5";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
   }
 }
